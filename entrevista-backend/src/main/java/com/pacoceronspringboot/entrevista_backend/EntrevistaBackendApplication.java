62b08f91bc1841da81357de0d762d460a41d4efe
Francisco Ceron <ceronlakai@gmail.com>
2026-03-21 18:47:40 -0600

    Initial commit

---
+++ entrevista-backend/src/main/java/com/pacoceronspringboot/entrevista_backend/EntrevistaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pacoceronspringboot.entrevista_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistaBackendApplication.class, args);
+	}
+
+}
---
+++ entrevista-backend/src/test/java/com/pacoceronspringboot/entrevista_backend/EntrevistaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacoceronspringboot.entrevista_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
